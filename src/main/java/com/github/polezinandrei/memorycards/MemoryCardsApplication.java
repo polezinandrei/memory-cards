b5cc7453456a78eef80bba03710f1252c53fa1ea
polezinandrei <polezinandrei@gmail.com>
2023-02-03 23:58:27 +0400

    MC-1: Create the skeleton of application

---
+++ src/main/java/com/github/polezinandrei/memorycards/MemoryCardsApplication.java
@@ -0,0 +1,13 @@
+package com.github.polezinandrei.memorycards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryCardsApplication.class, args);
+	}
+
+}
